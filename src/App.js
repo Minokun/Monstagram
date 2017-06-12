e6689571505abd88050064a525fe457c509cd8d1
Wu XiaoKun <952718180@qq.com>
2017-06-12 18:18:44 +0800

    完成展示列表主页面

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Icon , Row , Col} from 'antd';
+import { Icon , Row , Col , Dropdown , message , Menu } from 'antd';
+
+const onClick = function ({ key }) {
+  message.info(`Click on item ${key}`);
+};
+
+var cookie = true;
+
+const menu = (
+    <Menu onClick={(onClick)}>
+        {cookie ? <Menu.Item key="2"><Icon type="camera" /> 作品发布</Menu.Item> : <Menu.Item key="3"><Icon type="camera" /> 账号登录</Menu.Item>}
+    </Menu>
+);
 
 class AppHeader extends Component {
   render() {
@@ -9,13 +21,19 @@ class AppHeader extends Component {
       <div className="App">
         <div className="App-header">
           <Row>
-          <Col span={6}></Col>
+          <Col span={8}></Col>
           <Col span={12}>
             <img src={logo} className="App-logo" alt="logo" />
             <h2 className="App-title">Monstagram</h2>
           </Col>
-          <Col span={6}>
-            <span className="App-user"><Icon type="user" /></span>
+          <Col span={4}>
+            <span className="App-user">
+              <Dropdown overlay={menu}>
+                <a className="ant-dropdown-link" href="#">
+                  <Icon type="user" style={{ fontSize: 24 }}/>
+                </a>
+              </Dropdown>
+            </span>
           </Col>
           </Row>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import AppHeader from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import { Layout } from 'antd';
-const { Footer , Content } = Layout;
+import { Icon , Layout } from 'antd';
+import Praise from './Praise.js';
+import Comment from './Comment.js';
+
+const { Content } = Layout;
 
 
 
@@ -17,17 +20,39 @@ class AppLayout extends Component {
 					<AppHeader />
 					{/* 列表展示页面 */}
 					<Content className='App-content' >
-				    	<div className='Photo-list'>Content</div>
+						<div className='content-list'>
+
+							{/* 作者信息 */}
+							<div className='author'>
+								<Icon type="smile" />
+								<span className='author-content'>Minokun</span>
+								<span className='author-time'>3 min</span>
+							</div>
+
+							{/* 图片 */}
+					    	<div className='photo'>
+					    		<img className='pic' src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1497248911983&di=622726b7512324805e9c30bf51fe9444&imgtype=0&src=http%3A%2F%2Fandroid-wallpapers.25pp.com%2Ffs01%2F2014%2F09%2F30%2F0_3675c5ca51a2f5fec8f756217ea66dd6_900x675.jpg"  alt="百度推片" />
+					    	</div>
+
+					    	{/* 点赞信息 */}
+					    	<div className='info'>
+					    		<Praise />
+					    	</div>
+
+					    	{/* 评论信息 */}
+					    	<div className='comment'>
+					    		<h5 style={{paddingTop: 5 , paddingBottom: 5}}>世界的尽头</h5>
+					    		<Comment />
+					    	</div>
+
+						</div>
+						
 				    </Content>
-				    {/* 页脚 */}
-				    <Footer className='App-footer'>
-				    	Web Design ©2017 Created by MonstarLab Minok
-				    </Footer>
 			    </Layout>
 			</div>
 		);
 	}
 }
 
-ReactDOM.render(<AppLayout />, document.getElementById('header'));
+ReactDOM.render(<AppLayout />, document.getElementById('conainer'));
 registerServiceWorker();
